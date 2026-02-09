1c5c247e3a8d7526d86247aa9b739ce7c6a1a9ac
CaptainSharky <chuikostepan@mail.ru>
2026-02-09 16:01:31 +0300

    [Feature] TabBar Cart Tab

---
+++ iOS-FakeNFT-Extended/UI/Cart/CartView.swift
@@ -0,0 +1,18 @@
+//
+//  CartView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Stepan Chuiko on 09.02.2026.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartView()
+}
