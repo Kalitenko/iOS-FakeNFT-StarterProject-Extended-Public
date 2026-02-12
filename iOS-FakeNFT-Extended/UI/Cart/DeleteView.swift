aa39ff80c0f82719f62e27ff4f3affe600fb91db
CaptainSharky <chuikostepan@mail.ru>
2026-02-12 19:50:38 +0300

    [Refactor] DeleteView refactored

--- iOS-FakeNFT-Extended/UI/Cart/DeleteView.swift
+++ iOS-FakeNFT-Extended/UI/Cart/DeleteView.swift
@@ -14,46 +14,66 @@ struct DeleteView: View {
 
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(.ultraThinMaterial)
-                .ignoresSafeArea()
+            background
 
             VStack(spacing: 20) {
                 VStack(spacing: 12) {
-                    nft.image
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 108, height: 108)
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                    Text(L10n.Confirmations.deleteFromCartMessage)
-                        .font(.smallText)
-                        .foregroundStyle(.appTextPrimary)
-                        .multilineTextAlignment(.center)
+                    image
+                    confirmText
                 }
                 .frame(width: 180)
                 HStack(spacing: 8) {
-                    Button(action: onDelete) {
-                        Text(L10n.Confirmations.delete)
-                            .font(.bigText)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 44)
-                            .foregroundStyle(.appRed)
-                            .background(.appTextPrimary)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                    }
-                    Button(action: onCancel) {
-                        Text(L10n.Confirmations.back)
-                            .font(.bigText)
-                            .frame(maxWidth: .infinity)
-                            .frame(height: 44)
-                            .foregroundStyle(.appBackground)
-                            .background(.appTextPrimary)
-                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                    }
+                    deleteButton
+                    backButton
                 }
                 .padding(.horizontal, 56)
             }
         }
+    }
+
+    private var image: some View {
+        nft.image
+            .resizable()
+            .scaledToFit()
+            .frame(width: 108, height: 108)
+            .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+
+    private var confirmText: some View {
+        Text(L10n.Confirmations.deleteFromCartMessage)
+            .font(.smallText)
+            .foregroundStyle(.appTextPrimary)
+            .multilineTextAlignment(.center)
+    }
+
+    private var deleteButton: some View {
+        Button(action: onDelete) {
+            Text(L10n.Confirmations.delete)
+                .font(.bigText)
+                .frame(maxWidth: .infinity)
+                .frame(height: 44)
+                .foregroundStyle(.appRed)
+                .background(.appTextPrimary)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+    }
+
+    private var backButton: some View {
+        Button(action: onCancel) {
+            Text(L10n.Confirmations.back)
+                .font(.bigText)
+                .frame(maxWidth: .infinity)
+                .frame(height: 44)
+                .foregroundStyle(.appBackground)
+                .background(.appTextPrimary)
+                .clipShape(RoundedRectangle(cornerRadius: 12))
+        }
+    }
+
+    private var background: some View {
+        Rectangle()
+            .fill(.ultraThinMaterial)
+            .ignoresSafeArea()
     }
 }
 
