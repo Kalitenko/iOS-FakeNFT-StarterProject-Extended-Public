224f1f5f0ddfde353bab131a2c8c6d7ac4695bcc
Bogdan Kalitenko <kalitenkobv@gmail.com>
2026-02-16 23:31:50 +0500

    [Feature] add catalog requests

---
+++ iOS-FakeNFT-Extended/Services/Requests/NFTRequests.swift
@@ -0,0 +1,37 @@
+//
+//  NFTRequests.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Bogdan Kalitenko on 16.02.2026.
+//
+
+import Foundation
+
+struct NFTSRequest: NetworkRequest {
+    
+    let pageNumber: Int
+    let pageSize: Int
+    
+    init(pageNumber: Int = .zero, pageSize: Int = 15) {
+        self.pageNumber = pageNumber
+        self.pageSize = pageSize
+    }
+    
+    var endpoint: URL? {
+        var components = URLComponents(string: "\(RequestConstants.apiURL)/nft")
+        components?.queryItems = [
+            URLQueryItem(name: "\(RequestConstants.page)", value: "\(pageNumber)"),
+            URLQueryItem(name: "\(RequestConstants.size)", value: "\(pageSize)")
+        ]
+        return components?.url
+    }
+}
+
+struct NFTByIdRequest: NetworkRequest {
+    
+    let id: String
+    
+    var endpoint: URL? {
+        URL(string: "\(RequestConstants.apiURL)/nft/\(id)")
+    }
+}
