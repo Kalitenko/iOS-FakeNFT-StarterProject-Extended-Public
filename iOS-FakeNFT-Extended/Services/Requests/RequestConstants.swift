f22322503142aee6bcfe3efe982330a0aa166ab9
Stepan Chuiko <chuikostepan@mail.ru>
2026-02-16 21:52:18 +0300

    [Feature] Custom NavBar and private token added

--- iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
+++ iOS-FakeNFT-Extended/Services/Requests/RequestConstants.swift
@@ -1,5 +1,4 @@
 enum RequestConstants {
     static let baseURL = "https://d5dn3j2ouj72b0ejucbl.apigw.yandexcloud.net"
-#warning("insert your token here")
-    static let token = ""
+    static let token = "9151756d-9905-4c03-a1be-7dda2423cf97"
 }
---
+++ iOS-FakeNFT-Extended/UI/UIElements/ProgressView.swift
@@ -0,0 +1,7 @@
+//
+//  ProgressView.swift
+//  iOS-FakeNFT-Extended
+//
+//  Created by Stepan Chuiko on 16.02.2026.
+//
+
